064e644bdccdb428e9b056da58e78a4b4c658b8d
crumbcrumb <116420859+crumbcrumb@users.noreply.github.com>
2025-04-09 20:33:49 -0500

    added 3 js files and html file

---
+++ part1-conditionals.js
@@ -0,0 +1,29 @@
+//1. Grade Checker
+
+let score = 82;
+// Print A, B, C, D, or F based on score
+if score (>= 90) {
+    console.log("A")
+} else if score (>= 80) {
+    console.log("B")
+} else if score (>= 70) {
+    console.log("C")
+} else if score (>= 60) {
+    console.log("D")
+} else 
+console.log("F")
+
+
+
+//2. Pizza Quantity Warning
+
+let quantity = 12;
+// Print warning if quantity > 10
+
+
+
+
+//3. Login Simulator
+
+let password = "letmein"; // or use prompt
+// Print "Access granted" or "Access denied"
