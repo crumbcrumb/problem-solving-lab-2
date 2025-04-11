b7dc100bf00746e8575797bb67e11314c7c17fa3
crumbcrumb <116420859+crumbcrumb@users.noreply.github.com>
2025-04-10 20:01:19 -0500

    answered questions 1-3

--- part2-functions.js
+++ part2-functions.js
@@ -1,19 +1,29 @@
 //1. Dog Age Converter
 
-/*function dogToHumanYears(dogAge) {
-  
+function dogToHumanYears(dogAge) {
+  let humanYears = dogAge * 7;
+  return humanYears;
 }
-
+console.log(dogToHumanYears(1));
+console.log(dogToHumanYears(4));
+console.log(dogToHumanYears(7));
 
 //2. Tip Calculator
 
 function calculateTip(total, percentage) {
-  
+  let tip = total * percentage + total;
+  return tip;
 }
-
+console.log(calculateTip(1000, 0.17));
+console.log(calculateTip(289, 0.2));
+console.log(calculateTip(75, 0.15));
 
 //3. Even or Odd
 
 function checkEvenOdd(num) {
-  
-}*/
+  if (num % 2 === 0) return "even";
+  else return "odd";
+}
+console.log(checkEvenOdd(6));
+console.log(checkEvenOdd(197));
+console.log(checkEvenOdd(80));
